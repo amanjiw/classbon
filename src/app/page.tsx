0371fd7dc8f5e0d34ef786a796255684cec64648
amanjiw <ghaderi.amanj@gmail.com>
2023-11-23 11:43:48 +0330

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-
+import { CourseSummary } from "@/types/course-summary.interface";
 import HomeHeroSection from "./_components/home-hero-section/HomeHeroSection";
 
-export default function Home() {
+const getNewestCourses = async (count: number): Promise<CourseSummary[]> => {
+	const res = await fetch(
+		`https://api.classbon.com/api/courses/newest/${count}`,
+		{
+			next: { revalidate: 24 * 60 * 60 },
+		}
+	);
+	return res.json();
+};
+
+export default async function Home() {
+	const newestCourses = await getNewestCourses(3);
+
 	return (
 		<>
 			<HomeHeroSection />
