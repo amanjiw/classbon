f25f496df5ad75a2c4bf5c6d16367f750d4f37e9
amanjiw <ghaderi.amanj@gmail.com>
2023-12-30 19:32:40 +0330

    #

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -20,10 +20,26 @@ const getCourse = async (slug: string): Promise<CourseDetails> => {
 const CourseDetails = async ({ params }: { params: { slug: string } }) => {
 	const { slug } = params;
 
-	const courseData = await getCourse(slug);
-	console.log(courseData);
+	const course = await getCourse(slug);
+	console.log(course);
 
-	return <div> Course Details Page </div>;
+	return (
+		<div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10 ">
+			<div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>{" "}
+			<div className="col-span-10 xl:col-span-7">
+				<h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10 ">
+					{course.title}
+				</h1>
+				<h2 className="mt-4 text-center xl:text-right text-lg leading-9">
+					{course.subTitle}
+				</h2>
+				<div className="mt-5">VPC</div>
+			</div>
+			<div className="col-span-10 xl:col-span-3 bg-secondary"></div>
+			<div className="col-span-10 xl:col-span-6 bg-info"></div>
+			<div className="col-span-10 xl:col-span-4 bg-warning"></div>
+		</div>
+	);
 };
 
 export default CourseDetails;
