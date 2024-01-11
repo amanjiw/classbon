ebb307fde73fb88aec0c1c35c1e48043a2017a26
amanjiw <ghaderi.amanj@gmail.com>
2024-01-11 11:03:26 +0330

    tabs added

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import CourseAside from "@/app/(blog)/blog/[slug]/_components/CourseAside";
 import Progress from "@/app/_components/progress/Progress";
 import Rating from "@/app/_components/rating/Rating";
+import Tabs from "@/app/_components/tabs/Tabs";
 import { API_URL } from "@/configs/public";
 import { CourseDetails } from "@/types/course-details.interface";
+import { Tab } from "@/types/tab.type";
 import React from "react";
 
 export const getStaticParams = async () => {
@@ -26,6 +28,12 @@ const CourseDetails = async ({ params }: { params: { slug: string } }) => {
 	const course = await getCourse(slug);
 	console.log(course);
 
+	const tabs: Tab[] = [
+		{ label: "مشخصات دوره", content: course.description },
+		{ label: "دیدگاه ها و پرسش", content: "course comments" },
+		{ label: "سوالات متداول", content: " accordion components" },
+	];
+
 	return (
 		<div className=" container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10 ">
 			<div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>{" "}
@@ -41,7 +49,9 @@ const CourseDetails = async ({ params }: { params: { slug: string } }) => {
 			<div className="col-span-10 xl:col-span-3 ">
 				<CourseAside {...course} />
 			</div>
-			<div className="col-span-10 xl:col-span-6 bg-info"></div>
+			<div className="col-span-10 xl:col-span-6 ">
+				<Tabs tabs={tabs} />
+			</div>
 			<div className="col-span-10 xl:col-span-4 bg-warning"></div>
 		</div>
 	);
