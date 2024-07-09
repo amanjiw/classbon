e1f091a73f64560b123cc2b5df7b7e1f361769bd
amanji <ghaderi.amanj@gmail.com>
2024-07-09 07:53:27 +0330

    feat:next aut

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,45 @@
-import { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { createData } from "./core/http-service/http-service";
+import { VerifyUserModle } from "./app/(auth)/verify/_types/verify-user.type";
+import { User } from "./types/user.interface";
 
-export const authConfig = {
-	pages: {
-		signIn: "/signin",
-	},
+declare module "next-auth" {
+	interface User {
+		accessToken: string;
+	}
+}
 
-	callbacks: {},
-	providers: [],
-} satisfies NextAuthConfig;
+export const {
+	signIn,
+	signOut,
+	auth,
+	handlers: { GET, POST },
+} = NextAuth({
+	...authConfig,
+	providers: [
+		CredentialsProvider({
+			name: "credentials",
+			credentials: {
+				username: { label: "username", type: "text" },
+				code: { label: "code", type: "text" },
+			},
+			async authorize(credentials, request) {
+				try {
+					const user = await createData<VerifyUserModle, User>("", {
+						code: credentials.code as string,
+						username: credentials.username as string,
+					});
+
+					return {
+						accessToken: user.token,
+					};
+				} catch (error: unknown) {
+					console.log(error);
+					throw new Error("");
+				}
+			},
+		}),
+	],
+});
